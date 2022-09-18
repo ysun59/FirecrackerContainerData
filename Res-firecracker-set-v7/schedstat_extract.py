fc388c131ad836e637714f6a14207d25f1654320
ysun59 <ysun59@binghamton.edu>
2022-09-18 17:37:44 -0400

    add Res-firecracker-set-v7

---
+++ Res-firecracker-set-v7/schedstat_extract.py
@@ -0,0 +1,174 @@
+#For cat /proc/schedstat, use "schedstat_data_col_7.txt" to generate "schedstat_deltaData_col_7.txt"
+# to generate "schedstat_fig_col_7.png"
+#cpu0 0 0 0 0 0 0 2789857305742 120695662014 34103138 第七八九列数据，core 0~31
+
+
+import os
+import re
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+# from scipy import interpolate
+
+
+#取第N列的数据，横坐标：
+#core0第N列，core1第N列，core2第N列，...core31第N列 <第一秒数据>
+#core0第N列，core1第N列，core2第N列，...core31第N列 <第二秒数据>
+#.....
+#core0第N列，core1第N列，core2第N列，...core31第N列 <第60秒数据>
+#go into folder by itself
+folders = os.listdir('.')
+def schedstatNoColTxt(n):
+  for folder in folders:
+    # schedstat
+    cpu_perf_path = os.path.join('.', folder, 'schedstat.txt')
+    if os.path.exists(cpu_perf_path):
+      outputs = []
+
+      with open(cpu_perf_path, 'r') as file:
+        lines = file.readlines()
+
+        lineBuf = []
+        for line in lines:
+
+          a=line.split() #返回分割后的字符串列表list
+
+          # if line[0:3]=='cpu' or a[0:1] ==['version']:
+          if line[0:3]=='cpu':
+            # print('line is:')
+            # print(line)
+            if a[n:n+1]:
+              # print('a[n:n+1] is:')
+              # print(a[n:n+1])
+              # print(a[0:1])
+              # print(len(lineBuf))
+
+              if len(lineBuf)==0:
+                # print('first time')
+                lineBuf.append(''.join(a[n:n+1]))
+                # print(lineBuf)
+              else:
+                lineBuf.append(','.join(a[n:n+1]))
+                # print(lineBuf)
+
+              # if a[0:1] == ['cpu3'] and len(lineBuf) != 0:
+              # if a[0:1] == ['cpu3']:
+              if a[0:1] == ['cpu31']:
+                outputs.append(','.join(lineBuf))
+                # print('add')
+                # print(lineBuf)
+                lineBuf =[]
+              # print("#######")
+
+      mpstat_csv_path = os.path.join('.', folder, 'schedstat_data_col_{}.txt'.format(n))
+      with open(mpstat_csv_path, 'wt') as file:
+        file.write('\n'.join(outputs))
+
+schedstatNoColTxt(7) #取第N列数据生成txt，N从0开始
+schedstatNoColTxt(8) 
+schedstatNoColTxt(9) 
+
+
+#calculate delta, eg: delta of core 0 at No_1s - No_0s
+#core0第N列，core1第N列，core2第N列，...core31第N列 <第二秒数据 - 第一秒数据>
+#core0第N列，core1第N列，core2第N列，...core31第N列 <第三秒数据 - 第二秒数据>
+#.....
+#core0第N列，core1第N列，core2第N列，...core31第N列 <第60秒数据 - 第59秒数据>
+def schedstatDelta(n):
+  for folder in folders:
+    mpstat_csv_path = os.path.join('.', folder, 'schedstat_data_col_{}.txt'.format(n))
+    if os.path.exists(mpstat_csv_path):
+      outputs = []
+
+      with open(mpstat_csv_path, 'r') as file:
+        lines = file.readlines()
+
+        matrix = []
+        for line in lines:
+          items =[int(item) for item in line.split(',')]
+          matrix.append(items) #txt文件读到二维数组里面
+          # print(matrix)
+
+        lineBuf = []
+        # print(matrix[0][1])
+        rowNum = len(matrix) #行数
+        # print(rowNum)
+        colNum = len(matrix[0]) #列数
+        # print(colNum)
+        for i in range(0,rowNum - 1):
+          for j in range(0,colNum):
+            lineBuf.append(matrix[i+1][j] - matrix[i][j])
+            if j==colNum-1:
+              outputs.append(lineBuf)
+              lineBuf = []
+        # print(outputs)
+        mpstat_csv_path = os.path.join('.', folder, 'schedstat_deltaData_col_{}.txt'.format(n))
+        np.savetxt(mpstat_csv_path,outputs,fmt='%d',delimiter=',') #fmt='%.04f'为保留四位小数
+
+schedstatDelta(7)
+schedstatDelta(8)
+schedstatDelta(9)
+        
+
+
+
+
+
+
+# #draw pictures
+def schedstatNoColPng(n):
+  for folder in folders:
+    mpstat_csv_path = os.path.join('.', folder, 'schedstat_deltaData_col_{}.txt'.format(n))
+    # print("path")
+    # print(mpstat_csv_path)
+    if os.path.exists(mpstat_csv_path):
+      plt.style.use('ggplot') #图的风格bmh、ggplot、dark_background、fivethirtyeight和grayscale。
+
+      # print("exist path")
+      # print(mpstat_csv_path)
+      data = pd.read_table(mpstat_csv_path,sep=',',encoding='utf_8_sig',engine='python',header=None) #pd.read_table返回一个数据结构
+#       # T=data.T #行列转换
+      # print("第一行")
+      # print(data.iloc[0])  #打印第一行
+      rowZero=data.iloc[0] #打印第一行
+      # print("第一行有几个数字")
+      # print(len(rowZero))
+      colNum = len(rowZero) #第一行有几个数字
+      # print('colNum is')
+      # print(colNum)
+      date=data.values
+      date=data[0]
+      # print(date)
+      plt.cla() #清空坐标轴
+      x = range(0,len(data)) #60个数字60s
+      for i in range(0,colNum): #32个core
+      # for i in range(0,4): #32个core
+        plt.plot(x, data[i], '-', marker='o',label='cpu{}'.format(i)) #s-:方形,o-:圆形
+      plt.xlabel('time(s)') #横坐标名
+      plt.xticks(rotation=45)
+      plt.gca().yaxis.set_major_formatter(mticker.FormatStrFormatter('%d')) #gca就是get current axes的意思
+      plt.ylabel('time ms') #纵坐标名
+      plt.title('{}_col_{}'.format(folder,n))
+      plt.legend(loc='lower left', bbox_to_anchor=(1.01,0),ncol=2) #图例 (loc='best')自动选择放在合适位置，loc指的是我们的legend的左下角的那个顶点的坐标
+      plt.grid(True) #设置网格模式
+      sch_fig_path = os.path.join('.', folder, 'schedstat_fig_col_{}.png'.format(n))
+      # print(sch_fig_path)
+
+      fig=plt.figure(1) #解决1
+      fig.savefig(sch_fig_path, bbox_inches='tight') #解决1
+
+      # plt.savefig(sch_fig_path) #普通存图片方法
+      # plt.show()
+
+      # plt.cla() #清空坐标轴
+      # plt.clf()
+      # plt.close()
+
+      # plt.xticks(ticks=[i for i in range(3)], labels=[i for i in range(3)])
+      # plt.xticks(ticks=[2, 10, 20, 30], labels=[10, 20, 30, 40])
+      # plt.gca().xaxis.set_major_formatter(mticker.FormatStrFormatter('%.0fcpu'))
+
+schedstatNoColPng(7) #取第N列数据的txt生成png，N从0开始
+schedstatNoColPng(8)
+schedstatNoColPng(9)
